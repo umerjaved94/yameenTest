eb50d48134cc1d86dd55460ecf7cfc2b3d0325d0
Umer Javed <umerjaved94@hotmail.com>
2020-04-20 21:19:11 +0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function openSideBar() {
+  var element = document.getElementById("mobile_drawer");
+  element.classList.add("drawer_open");
+}
+
+function closeSideBar() {
+  var element = document.getElementById("mobile_drawer");
+  element.classList.remove("drawer_open");
+}
