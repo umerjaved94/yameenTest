bd86e6faa7778fffb1cecba796ff1eaaf8976639
umerjaved94 <umerjaved@umers-imac.local>
2020-04-23 18:19:16 +0500

    loader

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,8 @@ function closeSideBar() {
   var element = document.getElementById("mobile_drawer");
   element.classList.remove("drawer_open");
 }
+
+window.addEventListener("load", function(){
+	var loader = document.getElementById("preloader");
+	loader.className += "hidden"; 
+});
